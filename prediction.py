4006c8e753bc64d4c9c92a884d3dc83db89cf98d
binazon <kennyhulrich@yahoo.fr>
2021-05-25 11:51:18 +0200

    archiFix

--- prediction.py
+++ prediction.py
@@ -14,14 +14,12 @@ def predictNextDays(model_entry, array_enter, nbdays, scaled, look_back) -> List
             x_input = x_input.reshape(1,look_back, x_input.shape[1])
             yhat = model_entry.predict(x_input, batch_size=32, verbose = 2)
             yhat = scaled.inverse_transform(yhat)
-            #print("{} day output {}".format(i, yhat[0][0]))
             temp_input.append(yhat[0])
             temp_input = temp_input[1:]
         else:
             x_input = x_input.reshape(1,look_back, x_input.shape[1])
             yhat = model_entry.predict(x_input, batch_size=32, verbose = 2)
             yhat = scaled.inverse_transform(yhat)
-            #print("{} day output {} ".format(i,yhat[0][0]))
             temp_input.append(yhat[0])
         lst_output.append(max(0,yhat[0][0]))
         i=i+1
